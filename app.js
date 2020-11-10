f60b7096fad8beac18d93e04efe2ffb66e177437
Carlos García <a320605@uach.mx>
2020-11-10 16:51:02 -0700

    Gramatica y js
    Se agregaron un archivo js (main) y un txt con los detalles de la maquina

---
+++ app.js
@@ -0,0 +1,14 @@
+/*
+* Created by
+* Carlos Garcia 320605
+* Hector Aguirre 320793
+* Alejandro Aguirre 320646 
+* Victor Aguilar 320663
+*/ 
+
+
+function main() {
+    console.log("Listo, un 10 pls");
+}
+
+main()
